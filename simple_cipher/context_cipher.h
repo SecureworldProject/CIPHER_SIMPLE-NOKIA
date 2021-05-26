ef60f46acca170d304097faaaae7dea3d3aa8dfe
Sergio Molina <sergio.molina_cardin.ext@nokia.com>
2021-05-26 14:02:44 +0200

    Added struct KeyData and typedeffed unsigned char byte in context_cipher.h. Adeed 'struct KeyData* key' as parameter in cipher() and decipher().

--- simple_cipher/context_cipher.h
+++ simple_cipher/context_cipher.h
@@ -9,15 +9,32 @@
 
 
 
+#ifdef __cplusplus
+extern "C" {
+	#endif	//__cplusplus
 
-/////  STRUCTS AND ENUMS  /////
 
-struct Cipher {
-	char* id;
-	WCHAR* file_name;
-	HINSTANCE lib_handle;
-	int block_size;
-	char* custom;
-};
+	/////  STRUCTS AND ENUMS  /////
+
+	typedef unsigned char byte;
+
+	struct Cipher {
+		char* id;
+		WCHAR* file_name;
+		HINSTANCE lib_handle;
+		int block_size;
+		char* custom;
+	};
+
+	struct KeyData {
+		byte* data;
+		int size;
+		time_t expires;
+	};
+
+	#ifdef __cplusplus
+}
+#endif	//__cplusplus
+
 
 #endif // !CONTEXT_CIPHER_H
